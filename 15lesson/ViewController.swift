1ecb403892599709375e66a447843132c5265e0b
vladputsikovich <vladputsikovich@gmail.com>
2021-12-10 00:17:00 +0300

    Add figures

--- 15lesson/ViewController.swift
+++ 15lesson/ViewController.swift
@@ -10,41 +10,68 @@ import UIKit
 class ViewController: UIViewController {
 
     var views: [UIView] = []
+    var figures: [UIView] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        var newView: UIView = UIView()
-        
-        (0...7).forEach { xPos in
-            (0...7).forEach { yPos in
-                if xPos % 2 == 0 && yPos % 2 == 0 {
-                    newView = UIView(frame: CGRect(x: xPos * 40, y: yPos * 40 + 120, width: 40, height: 40))
-                    newView.backgroundColor = UIColor.black
-                    newView.autoresizingMask = [.flexibleRightMargin, .flexibleLeftMargin, .flexibleTopMargin, .flexibleBottomMargin, .flexibleHeight, .flexibleWidth]
-                    views.append(newView)
-                }
-                if xPos % 2 != 0 && yPos % 2 != 0 {
-                    newView = UIView(frame: CGRect(x: xPos * 40, y: yPos * 40 + 120, width: 40, height: 40))
-                    newView.backgroundColor = UIColor.black
-                    newView.autoresizingMask = [.flexibleRightMargin, .flexibleLeftMargin, .flexibleTopMargin, .flexibleBottomMargin, .flexibleHeight, .flexibleWidth]
-                    views.append(newView)
-                }
-                
-            }
-        }
-        views.forEach { view in
-            self.view.addSubview(view)
-            print(view.frame)
-        }
+        printDeck({ x, y in
+            appendView(x, y)
+        })
+        printDeck({ x, y in
+            appendFigures(x, y)
+        })
     }
     
     override func didRotate(from fromInterfaceOrientation: UIInterfaceOrientation) {
-        let colors: [UIColor] = [.black, .orange, .green, .blue]
-        let color = colors.randomElement()
-        self.views.forEach { view in
-            view.backgroundColor = color
+//        let colors: [UIColor] = [.black, .orange, .green, .blue]
+//        let color = colors.randomElement()
+//        self.views.forEach { view in
+//            view.backgroundColor = color
+//        }
+        printDeck({ x, y in
+            mixFigures(x, y)
+        })
+    }
+    
+    func printDeck(_ closure: (Int, Int) -> Void) {
+        (0...7).forEach { xPos in
+            (0...7).forEach { yPos in
+                if xPos % 2 == 0 && yPos % 2 == 0 {
+                    closure(xPos, yPos)
+                }
+                if xPos % 2 != 0 && yPos % 2 != 0 {
+                    closure(xPos, yPos)
+                }
+            }
         }
+    }
+    
+    func appendView(_ xPos: Int, _ yPos: Int) {
+        let view = UIView(frame: CGRect(x: xPos * 40, y: yPos * 40 + 120, width: 40, height: 40))
+        view.backgroundColor = .black
+        view.autoresizingMask = [.flexibleRightMargin, .flexibleLeftMargin, .flexibleTopMargin, .flexibleBottomMargin, .flexibleHeight, .flexibleWidth]
+        views.append(view)
+        self.view.addSubview(view)
+    }
+    
+    func appendFigures(_ xPos: Int, _ yPos: Int) {
+        let view = UIView(frame: CGRect(x: xPos * 40 + 10, y: yPos * 40 + 130, width: 20, height: 20))
+        if yPos < 3 {
+            view.backgroundColor = .red
+        }
+        if yPos > 4 {
+            view.backgroundColor = .white
+        }
+        view.autoresizingMask = [.flexibleRightMargin, .flexibleLeftMargin, .flexibleTopMargin, .flexibleBottomMargin, .flexibleHeight, .flexibleWidth]
+        figures.append(view)
+        self.view.addSubview(view)
+    }
+    
+    func mixFigures(_ xPos: Int, _ yPos: Int) {
+        guard let element = figures.randomElement() else {return}
+        element.frame.origin.x = CGFloat(xPos * 40 + 10)
+        element.frame.origin.y = CGFloat(yPos * 40 + 130)
     }
 }
 
